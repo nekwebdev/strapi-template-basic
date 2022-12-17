4f08f552dc4e3e61f1aaf13d58dfc51f00a1d051
nekwebdev <nek.webdev@gmail.com>
2022-12-16 14:27:49 -1000

    Add new data types

---
+++ template/src/api/home-parallax/controllers/home-parallax.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-parallax controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-parallax.home-parallax');
---
+++ template/src/api/home-parallax/routes/home-parallax.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-parallax router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-parallax.home-parallax');
